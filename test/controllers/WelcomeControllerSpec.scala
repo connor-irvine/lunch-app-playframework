1f017bb859dfd7c8fa0f1efc0f8b96322bc77bf8
Connor Irvine <connor.irvine@hmrc.digital.gov.uk>
2018-04-09 16:20:46 +0100

    add title and content to welcome page

--- test/controllers/WelcomeControllerSpec.scala
+++ test/controllers/WelcomeControllerSpec.scala
@@ -18,4 +18,16 @@ class WelcomeControllerSpec extends PlaySpec with GuiceOneAppPerTest {
     val home = route(app, request).get
     status(home) mustBe OK
   }
+
+  "return some html" in {
+    val controller = new WelcomeController
+    val result = controller.welcome.apply(FakeRequest())
+    contentType(result) mustBe Some("text/html")
+  }
+  "say hello and have a title" in {
+    val controller = new WelcomeController
+    val result = controller.welcome().apply(FakeRequest(GET, "/foo"))
+    contentAsString(result) must include ("<h1>Hello!</h1>")
+    contentAsString(result) must include ("<title>Welcome!</title>")
+  }
 }
